1d6d86eb21b0e4254d4635616ddc3cdc57cf1ddd
Davis <davis.ernst@outlook.com>
2026-04-05 00:28:23 -0600

    feat: install react-pdf, enable asyncWebAssembly

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [75, 90],
   },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
